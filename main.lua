e19780f9d98d29892ef536e6c68bfe3de733e0cd
cabana <usman.na.khan@gmail.com>
2017-07-01 23:59:20 -0700

    server code to handshake with a connected client

--- main.lua
+++ main.lua
@@ -1,12 +1,17 @@
 -- main.lua --
 
+-- Define serial number as a global
+s_no = "PB053117104000"
+
+-- Set up the softAP. This makes PBD appear
+-- as a WiFi network to anyone in range.
 function setupAP()
-   print("Setting up AP...")
+   print("setupAP()")
    
    -- Network Variables
    ap_cfg = 
    {
-      ssid = "PB053117104000",
+      ssid = s_no,
       pwd = "053117104000"
    }
 
@@ -29,8 +34,10 @@ function setupAP()
    wifi.ap.setip(ip_cfg)
 end
 
+-- Wait for a client to connect to the 
+-- softAP we have created
 function waitForClients()
-   print("Waiting for clients")
+   print("waitForClients()")
    
    tmr.alarm( 0, 1000, 1, function()
       --if wifi.sta.getip() == nil then
@@ -38,7 +45,7 @@ function waitForClients()
       table = {}
       table = wifi.ap.getclient()
       if next(table) == nil then
-         print("No cnxn for PBD setup...")
+         print("waitForClients(): No cnxn 4 PBD setup")
       else
          ip, nm, gw=wifi.ap.getip()
          print("IP Info: \nIP Address: ",ip)
@@ -49,26 +56,45 @@ function waitForClients()
             print(mac,ip)
          end
          tmr.stop(0)
+         beginServer()
+         
       end
    end) 
 end
 
 -- connects to a user's home WiFi network
--- Takes a config structure:
--- ap_cfg = 
--- {
---    ssid = "XXXXXXX...",
---    pass = "XXXXXXX..."
--- }
-function connectWifi(cfg)
-
+-- requires ssid and password
+function connectWifi(wifi_ssid, wifi_pwd)
+   print("connectWifi()")
+   --print("Connecting to home wifi AP")
+   station_cfg={}
+   station_cfg.ssid=wifi_ssid
+   station_cfg.pwd=wifi_pwd
 end
 
-
+-- Creates a server that will respond to HTTP
+-- requests from anybody that is connected
 function beginServer()
-
+   print("beginServer()")
+   srv=net.createServer(net.TCP)
+   srv:listen(8234,function(conn)
+      conn:on("receive",function(conn,payload)
+         print (payload)
+         if string.match(payload, string.reverse(s_no)) then
+            print ("mobile said hi, sending hi back")
+            conn:send("RX: " .. string.reverse(payload))
+         else
+            print ("unknown msg received")
+            conn:send("what? expected= " .. string.reverse(s_no))
+         end
+      end)
+      conn:on("sent",function(conn) 
+         conn:close() 
+      end)
+   end)
 end
 
+--
 function beginExistingSession()
 
 end  
@@ -77,7 +103,6 @@ end
 --       main() function        --
 ----------------------------------
 print("main()")
-
 setupAP()
    
 if(file.exists("env.cfg")) then
