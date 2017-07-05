87d74a801c23febed099969cf377ab1b1b092703
cabana <usman.na.khan@gmail.com>
2017-07-04 23:00:47 -0700

    receive companion packet from mobile-app and connect to the specified home wifi network

--- main.lua
+++ main.lua
@@ -59,19 +59,8 @@ function waitForClients()
          end
          tmr.stop(0)
          beginServer()
-         
       end
    end) 
-end
-
--- connects to a user's home WiFi network
--- requires ssid and password
-function connectWifi(wifi_ssid, wifi_pwd)
-   print("connectWifi()")
-   --print("Connecting to home wifi AP")
-   station_cfg={}
-   station_cfg.ssid=wifi_ssid
-   station_cfg.pwd=wifi_pwd
 end
 
 -- Creates a server that will respond to HTTP
@@ -102,6 +91,7 @@ function beginServer()
                conn:send("Thank you, byebye.")
                conn:on("sent",function(conn) 
                   conn:close() 
+                  connect2HomeWifi()
                end)
             end
          elseif count == 0 then
@@ -110,6 +100,75 @@ function beginServer()
          end
       end)
    end)
+end
+
+function connect2HomeWifi()
+   --register callbacks
+   wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
+      print("\n\tSTA - CONNECTED".."\n\tSSID: "..T.SSID.."\n\tBSSID: "..
+      T.BSSID.."\n\tChannel: "..T.channel)
+   end)
+
+   wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(T)
+      print("\n\tSTA - DISCONNECTED".."\n\tSSID: "..T.SSID.."\n\tBSSID: "..
+      T.BSSID.."\n\treason: "..T.reason)
+   end)
+
+   wifi.eventmon.register(wifi.eventmon.STA_AUTHMODE_CHANGE, function(T)
+      print("\n\tSTA - AUTHMODE CHANGE".."\n\told_auth_mode: "..
+      T.old_auth_mode.."\n\tnew_auth_mode: "..T.new_auth_mode)
+   end)
+
+   wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
+      print("\n\tSTA - GOT IP".."\n\tStation IP: "..T.IP.."\n\tSubnet mask: "..
+      T.netmask.."\n\tGateway IP: "..T.gateway)
+   end)
+
+   wifi.eventmon.register(wifi.eventmon.STA_DHCP_TIMEOUT, function()
+      print("\n\tSTA - DHCP TIMEOUT")
+   end)
+
+   wifi.eventmon.register(wifi.eventmon.AP_STACONNECTED, function(T)
+      print("\n\tAP - STATION CONNECTED".."\n\tMAC: "..T.MAC.."\n\tAID: "..T.AID)
+   end)
+
+   wifi.eventmon.register(wifi.eventmon.AP_STADISCONNECTED, function(T)
+      print("\n\tAP - STATION DISCONNECTED".."\n\tMAC: "..T.MAC.."\n\tAID: "..T.AID)
+   end)
+
+   wifi.eventmon.register(wifi.eventmon.AP_PROBEREQRECVED, function(T)
+      print("\n\tAP - PROBE REQUEST RECEIVED".."\n\tMAC: ".. T.MAC.."\n\tRSSI: "..T.RSSI)
+   end)
+
+   wifi.eventmon.register(wifi.eventmon.WIFI_MODE_CHANGED, function(T)
+      print("\n\tSTA - WIFI MODE CHANGED".."\n\told_mode: "..
+      T.old_mode.."\n\tnew_mode: "..T.new_mode)
+   end)
+
+   cfg = 
+   {
+      ssid = home_wifi_ssid,
+      pwd = home_wifi_psk
+   }
+   print("connect2HomeWifi()")
+   print("connecting to wifi ssid=" .. cfg.ssid .. " and pwd=" .. cfg.pwd)
+   wifi.setmode(wifi.STATION)
+   wifi.setphymode(wifi.PHYMODE_N)
+   wifi.sta.config(cfg)
+   wifi.sta.connect()
+end
+
+-- rotate servo to provide feed for pet (UNTESTED)
+function doServo()
+   local pin4 = 4
+   --setup at position 0
+   pwm.setup(pin4, 50, 71)
+   --turn to position -90
+   pwm.setduty(pin4, 27); pwm.start(D5); tmr.delay(500000); pwm.stop(D5)
+   --turn to position 0 again
+   pwm.setduty(pin4, 71); pwm.start(D5); tmr.delay(500000); pwm.stop(D5)
+   --turn to position +90
+   pwm.setduty(pin4, 123); pwm.start(D5); tmr.delay(500000); pwm.stop(D5)
 end
 
 --
@@ -128,5 +187,5 @@ if(file.exists("env.cfg")) then
 else
    print("no env file, start cfg")
    waitForClients()
+   print("finishing main...")
 end
-
