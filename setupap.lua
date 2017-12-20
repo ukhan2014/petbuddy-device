a2cceb6c03f5fa9406f90d0747bcc0c3cc339bf0
cabanaboi <ukhan2014@users.noreply.github.com>
2017-12-20 15:16:44 -0800

    Create setupap.lua

---
+++ setupap.lua
@@ -0,0 +1,27 @@
+local s_no, pass = ...
+print("setupAP(), s_no:" .. s_no .. " pass:" .. pass)
+
+-- Network Variables
+ap_cfg = 
+{
+   ssid = s_no,
+   pwd = pass
+}
+
+ip_cfg = 
+{
+   ip = "192.168.1.1",
+   netmask = "255.255.255.0",
+   gateway = "192.168.1.1"
+}
+
+-- Configure Wireless Internet
+wifi.setmode(wifi.SOFTAP)
+print('set mode=SOFTAP (mode='..wifi.getmode()..')\n')
+print('MAC Address: ',wifi.sta.getmac())
+print('Chip ID: ',node.chipid())
+print('Heap Size: ',node.heap(),'\n')
+
+-- Configure WiFi
+wifi.ap.config(ap_cfg)
+wifi.ap.setip(ip_cfg)
